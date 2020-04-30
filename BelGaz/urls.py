9b056ac3e1a55cb50083eb2afe96e7228e13dd2f
andrey-zubov <55183393+andrey-zubov@users.noreply.github.com>
2020-04-30 20:17:13 +0300

    Login view + template

--- BelGaz/urls.py
+++ BelGaz/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from planes.views import login
+
 from schema_graph.views import Schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("schema/", Schema.as_view()),
+    path('login/', login, name='login_url'),
 ]
--- planes/views.py
+++ planes/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+
+'''Autorization and auntification'''
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+
+from django.views.decorators.csrf import csrf_protect # https://docs.djangoproject.com/en/3.0/ref/csrf/
+
 
 # Create your views here.
+
+@csrf_protect
+def login(request):
+    template_name = 'login/login.html'
+    context = {}
+    if request.user.is_authenticated:
+        return HttpResponse('redirect somewhere')
+        # return redirect()
+    if request.method == 'POST':
+        password = request.POST['password']
+        login = request.POST['login']
+        user = auth.authenticate(username=login, password=password)
+        if user:
+            auth.login(request, user)
+            return HttpResponse('Залогинено успешно')
+            # return redirect()
+        else:
+            err_text = 'Неверный Email/Пароль'
+            context['error'] = err_text
+            return render(request, template_name, context)
+    return render(request, template_name, context)
